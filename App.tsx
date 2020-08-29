8f26d952c51651188b9424edea3c32ca44800733
Oliver H. Maceira <oliver@ciandt.com>
2020-08-29 20:17:47 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <Text>Hello World from Production!</Text>
       <StatusBar style="auto" />
     </View>
   );
